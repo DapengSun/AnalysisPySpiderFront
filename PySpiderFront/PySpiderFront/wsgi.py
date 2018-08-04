7811b8ac8e830900055141fe6a1fd10ed98e6acc
SunDapeng <13520387252@163.com>
2018-08-04 15:22:47 +0800

    修正路径404wenti

--- PySpiderFront/PySpiderFront/wsgi.py
+++ PySpiderFront/PySpiderFront/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import sys
+sys.path.append("/var/www/AnalysisPySpiderFront/PySpiderFront")
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PySpiderFront.settings")
 
 application = get_wsgi_application()
